a327d8c8ea4d1e69a51d88eeec7d3808f33800c1
IntanRatu03 <h1101211006@student.untan.ac.id>
2024-05-02 09:00:17 +0700

    modifikasi tgl pemupukan

--- resources/views/admin/kelolakomoditi/kelolajagung/modal/tanggalpemupukan.blade.php
+++ resources/views/admin/kelolakomoditi/kelolajagung/modal/tanggalpemupukan.blade.php
@@ -4,14 +4,13 @@
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Pilih tanggal pemupukan</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Pilih Tanggal Pemupukan</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body">
-            <div class="col-sm-8">
+          <div class="col-sm-8">
               <input type="date" class="form-control" id="tanggal_pupuk" name="tanggal_pupuk">
-            </div>
-          <div class="modal-footer">
+          </div>
+      <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="submit" class="btn btn-primary">Simpan</button>
       </div>
