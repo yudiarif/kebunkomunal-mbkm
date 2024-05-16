2ebe0be4e4eea7872041a51ac55ec3bc168e5d3a
sigitcp <sigit@student.untan.ac.id>
2024-05-16 13:52:38 +0700

    navbar notification ui

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -20,7 +20,33 @@
             @endif
         </h6>
     </div>
+
     <div class="navbar-nav align-items-center ms-auto">
+
+
+        <div class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                <div class="bg-success rounded-circle border border-2 border-white position-absolute bottom-3" style="padding: 0.35rem;"></div>
+                <i class="fa fa-bell me-lg-2 rounded-circle" style="width: 40px; height: 40px;" ></i>
+                <span class="d-none d-lg-inline-flex">Notification</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-end bg-light rounded justify-content-center p-2 pt-3">
+                <div class="justify-content-center p-1 rounded rounded-3 mb-2">
+                    <a href="#" class="dropdown-item">
+                        <h6 class="fw-normal mb-0">Profile updated</h6>
+                        <small>15 minutes ago</small>
+                    </a>
+                </div>
+                <div class="justify-content-center p-1 rounded rounded-3 mb-2">
+                    <a href="#" class="dropdown-item">
+                        <h6 class="fw-normal mb-0">Profile updated</h6>
+                        <small>15 minutes ago</small>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
         <div class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                 @if (auth()->user()->foto == NUll)
@@ -34,7 +60,7 @@
                 @endif
                 <span class="d-none d-lg-inline-flex"> {{ auth()->user()->nama }}</span>
             </a>
-            <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+            <div class="dropdown-menu dropdown-menu-end bg-light rounded justify-content-center p-2 m-0">
                 <!-- <a href="#" class="dropdown-item">My Profile</a> -->
                 <a href="{{ route('profil') }}" class="dropdown-item">Profil</a>
                 <a href="{{ route('logout') }}" class="dropdown-item">Log Out</a>
