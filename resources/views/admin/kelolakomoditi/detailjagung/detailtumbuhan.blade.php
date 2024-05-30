444170997c03d031ce51e22a0024db546a5a9bdd
IntanRatu03 <h1101211006@student.untan.ac.id>
2024-05-30 11:29:17 +0700

    add image skider

--- resources/views/admin/kelolakomoditi/detailjagung/detailtumbuhan.blade.php
+++ resources/views/admin/kelolakomoditi/detailjagung/detailtumbuhan.blade.php
@@ -1,42 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Slider</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .thumbnail-container {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+        .thumbnail {
+            cursor: pointer;
+            margin: 0 5px;
+        }
+        .thumbnail img {
+            border-radius: 5px;
+            width: 100px;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
 <div class="container-fluid pt-4 px-4">
     <div class="row g-4">
         <div class="col-sm-12 col-xl-6">
             <div class="bg-light rounded h-100 p-4">
                 <h6 class="mb-4">Detail Tanaman Jagung</h6>
 
-                <!-- fancybox -->
+                <!-- Bootstrap JS and dependencies -->
                 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
                 @if ($fotojagung)
-                <a href="{{ asset('storage/' . $fotojagung->foto1) }}" data-fancybox="jagung" data-caption="{{ $fotojagung->created_at }}">
-                    <img src="{{ asset('storage/' . $fotojagung->foto1) }}" style="border-radius: 15px; width: 100%;" alt="" class="zoomable-image">
-                </a>
-                <a href="{{ asset('storage/' . $fotojagung->foto2 ) }}" data-fancybox="jagung" data-caption="{{ $fotojagung->created_at }}"></a>
-                <a href="{{ asset('storage/' . $fotojagung->foto3 ) }}" data-fancybox="jagung" data-caption="{{ $fotojagung->created_at }}"></a>
-                @else
-                <h4>
-
-                    Belum Ada Foto
-                </h4>
-                @endif
-                    <!-- <a href="https://www.youtube.com/watch?v=Jup2IDm6NAs&ab_channel=Suksespedia" data-fancybox="jagung" data-caption="5 maret 2024"></a> -->
-
-                <!-- <div class="slider-container">
-                    <div class="slider">
-                        <div class="slide">
-                            <img src="img/jagung1.png" alt="1">
+                <div id="jagungCarousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="{{ asset('storage/' . $fotojagung->foto1) }}" class="d-block w-100" style="border-radius: 15px;" alt="">
+                            {{-- <div class="carousel-caption d-none d-md-block">
+                                <p>{{ $fotojagung->created_at }}</p>
+                            </div> --}}
                         </div>
-                        <div class="slide">
-                            <img src="img/jagung2.png" alt="2">
+                        <div class="carousel-item">
+                            <img src="{{ asset('storage/' . $fotojagung->foto2) }}" class="d-block w-100" style="border-radius: 15px;" alt="">
+                            {{-- <div class="carousel-caption d-none d-md-block">
+                                <p>{{ $fotojagung->created_at }}</p>
+                            </div> --}}
                         </div>
-                        <div class="slide">
-                            <img src="img/jagung3.png" alt="3">
-                        </div>
-                        <div class="slide">
-                            <img src="img/jagung4.png" alt="4">
+                        <div class="carousel-item">
+                            <img src="{{ asset('storage/' . $fotojagung->foto3) }}" class="d-block w-100" style="border-radius: 15px;" alt="">
+                            {{-- <div class="carousel-caption d-none d-md-block">
+                                <p>{{ $fotojagung->created_at }}</p>
+                            </div> --}}
                         </div>
                     </div>
-                    <input class="mt-3 mb-3" type="range" id="slider-progress" min="0" max="3" value="0" step="1">
-                </div> -->
+                    <a class="carousel-control-prev" href="#jagungCarousel" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#jagungCarousel" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+
+                <div class="thumbnail-container">
+                    <div class="thumbnail" data-target="#jagungCarousel" data-slide-to="0">
+                        <img src="{{ asset('storage/' . $fotojagung->foto1) }}" alt="">
+                    </div>
+                    <div class="thumbnail" data-target="#jagungCarousel" data-slide-to="1">
+                        <img src="{{ asset('storage/' . $fotojagung->foto2) }}" alt="">
+                    </div>
+                    <div class="thumbnail" data-target="#jagungCarousel" data-slide-to="2">
+                        <img src="{{ asset('storage/' . $fotojagung->foto3) }}" alt="">
+                    </div>
+                </div>
+                @else
+                <h4>Belum Ada Foto</h4>
+                @endif
+
             </div>
         </div>
+</body>
+</html>
