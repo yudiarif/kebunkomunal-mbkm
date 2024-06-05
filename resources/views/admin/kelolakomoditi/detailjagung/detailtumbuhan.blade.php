2d21a332f25e92fcbd7131b079818af7f57f8e6f
sigitcp <sigit@student.untan.ac.id>
2024-06-05 16:57:19 +0700

    fix: dashboard user, detai tanaman

--- resources/views/admin/kelolakomoditi/detailjagung/detailtumbuhan.blade.php
+++ resources/views/admin/kelolakomoditi/detailjagung/detailtumbuhan.blade.php
@@ -8,45 +8,46 @@
                 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
                 @if ($fotojagung)
-              <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="9000">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-             <a href="{{ asset('storage/' . $fotojagung->foto1) }}" data-fancybox="jagung" data-caption="{{ $fotojagung->created_at }}">
-                    <img src="{{ asset('storage/' . $fotojagung->foto1) }}" style="border-radius: 15px; width: 100%;" alt="" class="zoomable-image">
-                </a>
-        </div>
-        <div class="carousel-item">
-            <a href="{{ asset('storage/' . $fotojagung->foto2 ) }}" data-fancybox="jagung" data-caption="{{ $fotojagung->created_at }}">
-                <img src="{{ asset('storage/' . $fotojagung->foto2) }}" style="border-radius: 15px; width: 100%;" alt="" class="zoomable-image">
-                </a>
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="9000">
+                <div class="position-absolute p-2" style="z-index: 3;"><i class="fa fa-info-circle text-white" style="opacity: 90%;"></i></div>
+                    <div class="carousel-inner" >
+                        <div class="carousel-item active">
+                            <a href="{{ asset('storage/' . $fotojagung->foto1) }}" data-fancybox="jagung" data-caption="{{ $fotojagung->created_at }}">
+                                <img src="{{ asset('storage/' . $fotojagung->foto1) }}" style="border-radius: 15px; width: 100%; height: 230px;" alt="" class="zoomable-image">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="{{ asset('storage/' . $fotojagung->foto2 ) }}" data-fancybox="jagung" data-caption="{{ $fotojagung->created_at }}">
+                                <img src="{{ asset('storage/' . $fotojagung->foto2) }}" style="border-radius: 15px; width: 100%; height: 230px;" alt="" class="zoomable-image">
+                            </a>
 
-        </div>
-        <div class="carousel-item">
-            <a href="{{ asset('storage/' . $fotojagung->foto3 ) }}" data-fancybox="jagung" data-caption="{{ $fotojagung->created_at }}">
-                <img src="{{ asset('storage/' . $fotojagung->foto3) }}" style="border-radius: 15px; width: 100%;" alt="" class="zoomable-image">
-                </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="{{ asset('storage/' . $fotojagung->foto3 ) }}" data-fancybox="jagung" data-caption="{{ $fotojagung->created_at }}">
+                                <img src="{{ asset('storage/' . $fotojagung->foto3) }}" style="border-radius: 15px; width: 100%; height: 230px;" alt="" class="zoomable-image">
+                            </a>
 
-        </div>
-    </div>
-</div>
+                        </div>
+                    </div>
+                </div>
 
-<ol class="carousel-indicators  py-2 gap-2">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" data- class="active">
-        <img src="{{ asset('storage/' . $fotojagung->foto1 ) }}" class="d-block w-100" style="border-radius: 7px">
-    </li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1">
-        <img src="{{ asset('storage/' . $fotojagung->foto2 ) }}" class="d-block w-100" style="border-radius: 7px">
-        </li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2">
-        <img src="{{ asset('storage/' . $fotojagung->foto3 ) }}" class="d-block w-100" style="border-radius: 7px">
-        </li>
-</ol>
+                <ol class="carousel-indicators  py-2 gap-2">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" data- class="active">
+                        <img src="{{ asset('storage/' . $fotojagung->foto1 ) }}" class="d-block w-100" style="border-radius: 7px; height: 70px; width: 100%;">
+                    </li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1">
+                        <img src="{{ asset('storage/' . $fotojagung->foto2 ) }}" class="d-block w-100" style="border-radius: 7px; height: 70px; width: 100%;">
+                    </li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2">
+                        <img src="{{ asset('storage/' . $fotojagung->foto3 ) }}" class="d-block w-100" style="border-radius: 7px; height: 70px; width: 100%;">
+                    </li>
+                </ol>
 
                 @else
                 <h4>
                     Belum Ada Foto
                 </h4>
                 @endif
-             
+
             </div>
         </div>
--- resources/views/user/dashboard/jumlah.blade.php
+++ resources/views/user/dashboard/jumlah.blade.php
@@ -1,6 +1,6 @@
 <!-- Sale & Revenue Start -->
 <div class="svg" style="z-index: 1;">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150" id="my-svg" >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150" id="my-svg">
         <linearGradient id="my-gradient" x1="0%" y1="100%" x2="0%" y2="0%">
             <stop offset="0%" style="stop-color:rgb(88,168,24);stop-opacity:1" />
             <stop offset="50%" style="stop-color:rgb(137,188,86);stop-opacity:1" />
@@ -32,26 +32,34 @@
                 </div>
             </div>
         </div> -->
+
         <div class="col-12 col-xl-3" style="z-index: 3;">
-            <div class="bg-light rounded d-flex justify-content-center p-4">
-                <i class="fa fa-seedling fa-5x text-primary mt-2"></i>
-                <div class="ms-3">
-                    <h5 class="mb-1">Jagung</h5>
-                    <h1 class="mb-0" style="color: green;">
-                        @if (!optional($totaljagung->last())->jumlah_slot == NULL)
-                        {{ optional($totaljagung->last())->jumlah_slot }}
-                        @else
-                        0
-                        @endif
-                    </h1>
-                    <h7>Slot</h7>
+            <a href="#" data-toggle="modal" data-target="#InformasislotModal">
+                <div class="position-absolute p-2"><i class="fa fa-info-circle text-body" style="opacity: 50%;"></i></div>
+                <div class="bg-light rounded d-flex justify-content-center p-4">
+                    <i class="fa fa-seedling fa-6x text-primary mt-0"></i>
+                    <div class="ms-3">
+                        <h5 class="mb-1 d-none d-md-block">Jagung</h5>
+                        <h2 class="mb-1 d-block d-md-none">Jagung</h2>
+                        <h1 class="mb-0" style="color: green;">
+                            @if (!optional($totaljagung->last())->jumlah_slot == NULL)
+                            {{ optional($totaljagung->last())->jumlah_slot }}
+                            @else
+                            0
+                            @endif
+                        </h1>
+                        <h7 class="text-body d-none d-md-inline">Slot</h7>
+                        <h5 class="text-body d-inline d-md-none">Slot</h5>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
+
 
         <div class="col-12 col-xl-9" style="z-index: 3;">
             <div class="bg-light rounded d-flex justify-content-center p-3">
-            <div id="id3e4d36977a684" a='{"t":"r","v":"1.2","lang":"id","locs":[1679],"ssot":"c","sics":"ds","cbkg":"rgba(137,188,86,0)","cfnt":"rgba(137,188,86,1)","codd":"#FFFFFF00","cont":"rgba(137,188,86,1)","eln":"bool","stof":"3"}'>Sumber Data Cuaca: <a href="https://cuacalab.id/cuaca_pontianak/besok/">ramalan cuaca besok di Pontianak</a></div><script async src="https://static1.cuacalab.id/widgetjs/?id=id3e4d36977a684"></script>
+                <div id="id3e4d36977a684" a='{"t":"r","v":"1.2","lang":"id","locs":[1679],"ssot":"c","sics":"ds","cbkg":"rgba(137,188,86,0)","cfnt":"rgba(137,188,86,1)","codd":"#FFFFFF00","cont":"rgba(137,188,86,1)","eln":"bool","stof":"3"}'>Sumber Data Cuaca: <a href="https://cuacalab.id/cuaca_pontianak/besok/">ramalan cuaca besok di Pontianak</a></div>
+                <script async src="https://static1.cuacalab.id/widgetjs/?id=id3e4d36977a684"></script>
             </div>
         </div>
 
@@ -87,6 +95,40 @@
                 </div>
             </div>
         </div> -->
+    </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<div class="modal fade" id="InformasislotModal" tabindex="-1" role="dialog" aria-labelledby="InformasislotModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title" id="InformasislotModalLabel">Pertumbuhan Tinggi Tanaman</h6>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="bg-light d-flex flex-column flex-md-row justify-content-center align-items-center p-4 text-center text-md-left">
+                    <i class="fa fa-calendar-check fa-5x text-primary mb-2 mb-md-0 me-0 me-md-3"></i>
+                    <div class="mt-2">
+                        <h6 class="mb-1">Priode Jagung Anda Dimulai Pada</h6>
+                        <h4 class="mb-0 text-primary">
+                            @if (!optional($totaljagung->last())->jumlah_slot == NULL)
+                            {{ optional($tanggalmulai->last())->created_at->format('Y-m-d') }}
+                            @else
+                            0
+                            @endif
+                        </h4>
+                        <h7>Dengan Kuantitas Sebanyak <span class="text-primary">{{ optional($totaljagung->last())->jumlah_slot }}</span> Slot</h7>
+                    </div>
+                </div>
+
+            </div>
+            <!-- Tambahan jika diperlukan: Tombol Simpan, Tombol Tutup, dll. -->
+        </div>
     </div>
 </div>
 <!-- Sale & Revenue End -->
