25e48827c2656747a679d8d53849d2d4f37cc23c
yudiarif <yudiarif@student.untan.ac.id>
2024-06-05 18:39:31 +0700

    fix : modal dashboard

--- resources/views/user/dashboard/jumlah.blade.php
+++ resources/views/user/dashboard/jumlah.blade.php
@@ -105,7 +105,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h6 class="modal-title" id="InformasislotModalLabel">Pertumbuhan Tinggi Tanaman</h6>
+                {{-- <h6 class="modal-title" id="InformasislotModalLabel">Pertumbuhan Tinggi Tanaman</h6> --}}
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
